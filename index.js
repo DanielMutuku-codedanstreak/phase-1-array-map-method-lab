89f1c881b21496d16525172d2e570c23da518c7b
Daniel Mutuku <danielmakau507@gmail.com>
2023-10-01 08:50:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,16 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    // Split the tutorial into words
+    const words = tutorial.split(' ');
+
+    // Capitalize the first letter of each word
+    const titleCaseWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+
+    // Join the words back into a title case tutorial
+    const titleCaseTutorial = titleCaseWords.join(' ');
+
+    return titleCaseTutorial;
+  });
 }
